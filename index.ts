7992252d9559662cebb008b286f1b785263ee1de
samuel21695 <samuel21695@gmail.com>
2023-12-13 21:42:21 +0900

    📝  private explanation comments

--- index.ts
+++ index.ts
@@ -28,3 +28,11 @@ let memberDataWithoutClosure: MemberData = {
   address: 'address2',
   cart: ['item3', 'item4']
 };
+/**
+ * 위의 객체는 클로저를 사용하지 않았기 때문에, memberDataWithoutClosure.id, memberDataWithoutClosure.password 등으로 직접 접근할 수 있습니다.
+ * 개발할 때는 접근하는 것이 편리하지만, 이후에 유지보수를 할 때는 위험한 방법입니다.
+ * 객체지향 프로그래밍에서는 은닉화 작업을 통해 직접 접근을 막습니다.
+ * 하지만 함수형 프로그램밍에서는 일반적인 방식으로는 은닉화를 할 수 없기 때문에
+ * 클로저 패턴, 위 예제에서 memberDataWithClosure 함수를 통해 은닉화를 구현합니다.
+ */
+
